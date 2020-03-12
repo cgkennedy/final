2c16a4c321413a7151e7146d8afbd03db8a76dea
cgkennedy <curt.kennedy@kellogg.northwestern.edu>
2020-03-12 21:03:24 +0000

    final

--- createdb.rb
+++ createdb.rb
@@ -5,31 +5,45 @@ DB = Sequel.connect(connection_string)                                          
 #######################################################################################
 
 # Database schema - this should reflect your domain model
-DB.create_table! :events do
+DB.create_table! :attractions do
   primary_key :id
   String :title
   String :description, text: true
-  String :date
+  String :current_conditions
   String :location
 end
-DB.create_table! :rsvps do
+DB.create_table! :ratings do
   primary_key :id
-  foreign_key :event_id
-  Boolean :going
+  foreign_key :attraction_id
+  Boolean :attended_this_week
   String :name
   String :email
   String :comments, text: true
 end
+DB.create_table! :users do
+  primary_key :id
+  String :name
+  String :email
+  String :password
+end
 
 # Insert initial (seed) data
-events_table = DB.from(:events)
+attractions_table = DB.from(:attractions)
 
-events_table.insert(title: "Bacon Burger Taco Fest", 
+events_table.insert(title: "Old Faithful", 
                     description: "Here we go again bacon burger taco fans, another Bacon Burger Taco Fest is here!",
                     date: "June 21",
                     location: "Kellogg Global Hub")
 
-events_table.insert(title: "Kaleapolooza", 
+events_table.insert(title: "Yellowstone Lake", 
                     description: "If you're into nutrition and vitamins and stuff, this is the event for you.",
                     date: "July 4",
                     location: "Nowhere")
+
+events_table.insert(title: "Grand Prismatic Spring", 
+                    description: "If you're into nutrition and vitamins and stuff, this is the event for you.",
+                    date: "July 4",
+                    location: "Nowhere")
+
+
+
